81282dcf3d58681eff865f03d415e2aa52fcd9d6
iiKurt <ikurt018@gmail.com>
2020-12-10 15:52:45 +1300

    Move graphics logic from main kernel function to its own class

---
+++ Source/Kernel/Source/BasicRenderer.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Point.hpp"
+#include <Framebuffer.h>
+#include <PSF.h>
+
+class BasicRenderer {
+public:
+	Point CursorPosition;
+	Framebuffer* FB;
+	PSF1_FONT* Font;
+	unsigned int Color;
+
+	BasicRenderer(Framebuffer* fb, PSF1_FONT* font);
+
+	void PutChar(char chr, unsigned int xOff, unsigned int yOff);
+	void PrintStr(const char* str);
+};
