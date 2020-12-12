6ee91f0356ad74a7f8be00753e809c2330956105
iiKurt <ikurt018@gmail.com>
2020-12-13 12:12:52 +1300

    Read EFI memory map from kernel

--- Source/Kernel/Source/Kernel.cpp
+++ Source/Kernel/Source/Kernel.cpp
@@ -3,6 +3,7 @@
 #include "Painter.hpp"
 #include "Console.hpp"
 #include "String.hpp"
+#include "EFIMemory.hpp"
 
 extern "C" void _start(BootInfo* bootInfo) {
 	Painter p = Painter(bootInfo->fb, bootInfo->font);
@@ -19,9 +20,26 @@ extern "C" void _start(BootInfo* bootInfo) {
 	c.PrintLine("");
 	c.ForegroundColor = 0xFFFFFFFF;
 
-	for (int i = 0; i < 40; i++) {
-		c.Print("Number: ");
-		c.PrintLine(to_string((int64_t)i));
+	// Print EFI memory sections
+	// Number of map entries
+	uint64_t mMapEntries = bootInfo->mMapSize / bootInfo->mMapDescriptorSize;
+
+	// Iterate through each map entry
+	for (unsigned int i = 0; i < mMapEntries; i++) {
+		// Get the descriptor at an offset to the memory map
+		EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)((uint64_t)bootInfo->mMap + (i * bootInfo->mMapDescriptorSize));
+		
+		// Print the corrosponding type string to the console
+		c.Print(EFI_MEMORY_TYPE_STRINGS[desc->type]);
+		c.ForegroundColor = 0xFFFF00FF;
+		c.Print(" ");
+		// pageCount is the number of 4KiB pages
+		// Calculate how many KiBs by multiplying by 4
+		c.Print(to_string(desc->pageCount << 2));
+		// Do ((pageCount * 4) * 1024)/1000 to calculate kB
+		// Or ((pageCount * 4096)/1000)
+		c.PrintLine(" KiB");
+		c.ForegroundColor = 0xFFFFFFFF;
 	}
 
 	for (;;) { __asm__("cli; hlt"); } // Halt the system
