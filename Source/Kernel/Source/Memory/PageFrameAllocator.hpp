2e1ac84d6a76f27fa2803fc5f193f68d758ed38e
iiKurt <ikurt018@gmail.com>
2020-12-14 12:20:04 +1300

    Page frame allocator
    big fun

--- Source/Kernel/Source/Memory/PageFrameAllocator.hpp
+++ Source/Kernel/Source/Memory/PageFrameAllocator.hpp
@@ -9,7 +9,23 @@
 class PageFrameAllocator {
 private:
 	void InitBitmap(size_t bitMapSize, void* bufferAddress);
+	void UnreservePage(void* address);
+	void UnreservePages(void* address, uint64_t pageCount);
+	void ReservePage(void* address);
+	void ReservePages(void* address, uint64_t pageCount);
+
 public:
 	Bitmap PageBitmap;
 	void ReadEFIMemoryMap(EFI_MEMORY_DESCRIPTOR* mMap, size_t mMapSize, size_t mMapDescriptorSize);
+
+	void* RequestPage();
+
+	void FreePage(void* address);
+	void FreePages(void* address, uint64_t pageCount);
+	void LockPage(void* address);
+	void LockPages(void* address, uint64_t pageCount);
+
+	uint64_t GetFreeRAM();
+	uint64_t GetUsedRAM();
+	uint64_t GetReservedRAM();
 };
