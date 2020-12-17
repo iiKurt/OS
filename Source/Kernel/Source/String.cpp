9e38f6dcf847b5cd6050e7d1f24dcd466ffc593d
iiKurt <ikurt018@gmail.com>
2020-12-17 21:19:10 +1300

    Amazing new to_string function, optimisations don't break code
    (Not sarcasm!)
    I made it myself!
    This new to_string function doesn't appear to break code
    When compiling with optimisations, the used ram decreases by 8KB!
    
    Also added some stuff to TODO.txt

--- Source/Kernel/Source/String.cpp
+++ Source/Kernel/Source/String.cpp
@@ -1,135 +1,100 @@
 #include "String.hpp"
 
-char stringOutput[128];
+// Say hello to probably to most well commented file in the entire project.
 
-// TODO: perhaps somehow combine both uint64 and int64 to_string logic
-const char* to_string(uint64_t value) {
-	uint8_t size;
-	uint64_t sizeTest = value;
+#include <stddef.h>
 
-	// For each time sizeTest can be divided by 10, increment size
-	// Could use modulus and a for loop to do this
-	while (sizeTest / 10 > 0) {
-		sizeTest /= 10;
-		size++;
+char str[128]; // Buffer overflows?!
+const char* to_string(int64_t num, int base, unsigned int desiredLength, bool prefix) {
+	if (base < 2 || base > 36) { // no
+		return NULL;
 	}
 
-	uint8_t index = 0;
+	// We don't know the length of the string - calculate it.
+	if (desiredLength <= 0) {
+		// Set length to one, as length calculation exits when stripping rightmost (remaining) digit
+		// results in zero (but there is still one digit yet to be calculated - so we compensate for this:)
+		int length = 1;
+		int lengthTest = num;
 
-	while (value / 10 > 0) {
-		uint8_t remainder = value % 10;
-		value /= 10;
-		stringOutput[size - index] = remainder + '0';
-		index++;
+		// lengthTest / base will only equal zero when the number is at 1 digit long, and we therefore know the length.
+		// lengthTest is implicity converted to int each time, so decimals are stripped off.
+		while (lengthTest / base != 0) { // While stripping the rightmost digit results in a non-zero digit.
+			lengthTest /= base; // Strip rightmost digit.
+			length++;
+		}
+
+		desiredLength = length;
 	}
 
-	uint8_t remainder = value % 10;
-	stringOutput[size - index] = remainder + '0';
-	stringOutput[size + 1] = 0; // Null-terminate the string
-	return stringOutput;
-}
-
-const char* to_string(int64_t value) {
-	uint8_t isNegative = 0;
-
-	if (value < 0) {
-		isNegative = 1;
-		value *= -1;
-		// For all intents and purposes it can be positive
-		// Prepend a - to the returned characters
-		stringOutput[0] = '-';
+	// If longest possible string output would result in buffer overflow.
+	// 4 because of: -, 0, [prefix symbol], \0
+	if (desiredLength + 4 > 128) {
+		return NULL;
 	}
 
-	uint8_t size;
-	uint64_t sizeTest = value;
+	// Get a pointer to the end of the string.
+	// If we want to prepend something, we can increment this pointer to offset any additonal characters.
+	char* current = &str[0]; // Get a pointer to the start of the string.
 
-	while (sizeTest / 10 > 0) {
-		sizeTest /= 10;
-		size++;
+	if (num < 0) 
+	{
+		num = -num; // Treat it as a normal number
+		// Prepend '-'. This will set a character to the start of the string, then offset additional characters
+		*current++ = '-';
 	}
 
-	uint8_t index = 0;
-
-	while (value / 10 > 0) {
-		uint8_t remainder = value % 10;
-		value /= 10;
-		stringOutput[isNegative + size - index] = remainder + '0';
-		index++;
+	if (prefix && base != 10) { // Base 10 doesn't need a prefix
+		*current++ = '0';
+		switch (base) {
+			case 16: // Hexadecimal
+				*current++ = 'x';
+				break;
+			case 8: // Octal
+				*current++ = 'o';
+				break;
+			case 2: // Binary
+				*current++ = 'b';
+				break;
+			default: // Some strange unknown base
+				*current++ = '?';
+				break;
+		}
 	}
 
-	uint8_t remainder = value % 10;
-	stringOutput[isNegative + size - index] = remainder + '0';
-	stringOutput[isNegative + size + 1] = 0; // Null-terminate the string
-	return stringOutput;
-}
+	// Grab the address of where the prefixes are (subtracting 1 because that has not been filled yet).
+	char* endPrefixes = current - 1;
 
-const char* to_string(double value, uint8_t decimalPlaces) {
-	if (decimalPlaces > 20) {
-		decimalPlaces = 20;
+	// We could be working in reverse, so set the current position to the end of the string.
+	// Indexing starts from zero, so subtract one.
+	current += (desiredLength - 1);
+	
+	if (num == 0) { // Handle zero explicity, otherwise nothing occurs
+		*current-- = '0';
+	}
+	else {
+		// Filling string from right to left.
+		while (num != 0 && endPrefixes < current)
+		{
+			int rem = num % base; // Get rightmost digit.
+			// If number we want (rem) can't be represented in characters from 0-9, start counting from A.
+			// As the range 0-Z is not contiguous within ASCII, if we want to use A-Z, subtract 10 first and begin offsetting.
+			// Otherwise, just offset rem from the 0 character to find the desired symbol.
+			// Could do rem + (rem > 9 ? 55 : '0');
+			*current-- = (rem > 9) ? (rem - 10) + 'A' : rem + '0';
+			num = num/base; // 'Remove' rightmost digit by implicitly converting to an int, stripping the decimal.
+		}
 	}
 
-	char* intPtr = (char*)to_string((int64_t)value);
-	char* doublePtr = stringOutput;
-
-	if (value < 0) {
-		value *= -1;
+	// Detect if there is a gap between the start/prefix and digits
+	while (endPrefixes != current) {
+		*current-- = '0';
 	}
 
-	while (*intPtr != 0) {
-		*doublePtr = *intPtr;
-		intPtr++;
-		doublePtr++;
-	}
-
-	*doublePtr = '.';
-	doublePtr++;
-
-	double newValue = value - (int)value;
-
-	for (uint8_t i = 0; i < decimalPlaces; i++) {
-		newValue *= 10;
-		*doublePtr = (int)newValue + '0';
-		newValue -= (int)newValue;
-		doublePtr++;
-	}
-
-	*doublePtr = 0;
-	return stringOutput;
-}
-
-const char* generic_to_hex_string(uint8_t* valPtr, uint8_t size) {
-	size -= 1;
-	uint8_t* ptr;
-	uint8_t temp;
-
-	for (uint8_t i = 0; i < size; i++) {
-		ptr = (valPtr + i);
-		temp = ((*ptr & 0xF0) >> 4);
-		stringOutput[2 + size - (i * 2 + 1)] = temp + (temp > 9 ? 55 : '0');
-		temp = ((*ptr & 0x0F));
-		stringOutput[2 + size - (i * 2)] = temp + (temp > 9 ? 55 : '0');
-	}
-
-	// These have to be at the bottom of this function
-	stringOutput[0] = '0';
-	stringOutput[1] = 'x';
-
-	stringOutput[2 + size + 1] = 0;
-	return stringOutput;
-}
-
-const char* to_hex_string(uint64_t value) {
-	return generic_to_hex_string((uint8_t*)&value, 16); // Will only show last 16 characters
-}
-
-const char* to_hex_string(uint32_t value) {
-	return generic_to_hex_string((uint8_t*)&value, 8);
-}
-
-const char* to_hex_string(uint16_t value) {
-	return generic_to_hex_string((uint8_t*)&value, 4);
-}
-
-const char* to_hex_string(uint8_t value) {
-	return generic_to_hex_string((uint8_t*)&value, 2);
+	// Even though the str buffer is initally filled with \0's, we still need to add one at the end
+	// due to the buffer being shared across several function calls.
+	// Offsetting by endPrefixes because desiredLength doesn't account for prefixes.
+	// current pointer is not at the end of the array due to it being decremented to fill the array in reverse.
+	*(endPrefixes + desiredLength + 1) = '\0';
+	return str;
 }
